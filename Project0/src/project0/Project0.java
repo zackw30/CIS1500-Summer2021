6ac41bb5e37ff39ddcd47ae22eecbd7c71f828ff
zackw30 <warnke.zachary@student.oaklandcc.edu>
2021-05-20 17:14:02 -0400

    Project 0 Done

---
+++ Project0/src/project0/Project0.java
@@ -0,0 +1,16 @@
+
+package project0;
+
+public class Project0 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Zack Warnke");
+        System.out.println("He/ him/ his");
+        System.out.println("Computer Science");
+        System.out.println("It's an introductory course to get me on track for the fall semester.");
+        System.out.println("I hope to learn a lot of the bascis of java for future classes.");
+        System.out.println("I love video games, pets, sports, and I'm very competitive.");  
+    }
+    
+}
